3984651b4b8d51d0a38a6d74776f67d1f1fcdfba
kumin <273423480@qq.com>
2018-10-21 16:03:57 +0800

    修改dist可以上传

---
+++ precache-manifest.f19fe12665e0650029bda8c0363c9d6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c862f565a4e19b7aec38",
+    "url": "/js/chunk-vendors.59645627.js"
+  },
+  {
+    "revision": "12ed628675c5c2f7e2ae",
+    "url": "/js/app.90ef5d54.js"
+  },
+  {
+    "revision": "d2a5a3958a4677960dd9",
+    "url": "/js/about.72f7e6cd.js"
+  },
+  {
+    "revision": "b02fdd1f68d28ecb0837eeab348704c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "12ed628675c5c2f7e2ae",
+    "url": "/css/app.a07cbf35.css"
+  }
+];
