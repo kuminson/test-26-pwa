a47cadbb3365ed011225ab0f3cc67db94ae65584
kumin <273423480@qq.com>
2018-10-21 16:15:18 +0800

    修改打包路径

---
+++ precache-manifest.55d10bc02fa771a5d03382472b695055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-26-pwa/robots.txt"
+  },
+  {
+    "revision": "c862f565a4e19b7aec38",
+    "url": "/test-26-pwa/js/chunk-vendors.59645627.js"
+  },
+  {
+    "revision": "6d756c8195f72aadd747",
+    "url": "/test-26-pwa/js/app.644a110a.js"
+  },
+  {
+    "revision": "d2a5a3958a4677960dd9",
+    "url": "/test-26-pwa/js/about.72f7e6cd.js"
+  },
+  {
+    "revision": "6c5604bd825251899849699865b238e7",
+    "url": "/test-26-pwa/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/test-26-pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6d756c8195f72aadd747",
+    "url": "/test-26-pwa/css/app.a07cbf35.css"
+  }
+];
